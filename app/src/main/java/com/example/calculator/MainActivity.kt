f3b4ed066c117ff872c9f35de54e0c8e5dbb0ab2
Jay Shah <jay98a@gmail.com>
2022-06-02 13:00:42 -0400

    UI and style

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_clear : Button  = findViewById(R.id.btn_Clear);
+
+        btn_clear.setOnClickListener {
+            val toast = Toast.makeText(this,"Clear button",Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
     }
 }
